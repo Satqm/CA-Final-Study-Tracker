6a8f299e22c9e2224e255baaff06ce0a71da91ce
Satqm <satqm2991@gmail.com>
2026-01-01 02:00:09 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,33 +1,28 @@
-// Import the functions you need from the SDKs you need
+// Import Firebase using CDN (not modules)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { 
-  getAuth, 
-  createUserWithEmailAndPassword, 
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
-  signOut 
+    getAuth, 
+    createUserWithEmailAndPassword, 
+    signInWithEmailAndPassword,
+    onAuthStateChanged,
+    signOut 
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { 
-  getFirestore,
-  collection,
-  addDoc,
-  getDocs,
-  updateDoc,
-  doc,
-  query,
-  where,
-  setDoc,
-  getDoc
+    getFirestore,
+    doc,
+    setDoc,
+    getDoc,
+    updateDoc
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBVUi-vAyvE-g_iK4W9zbTTa5Ps8MwZEtg",
-  authDomain: "ca-final-537aa.firebaseapp.com",
-  projectId: "ca-final-537aa",
-  storageBucket: "ca-final-537aa.firebasestorage.app",
-  messagingSenderId: "156489737512",
-  appId: "1:156489737512:web:f8b06ec7cba71ddfe99c1d"
+    apiKey: "AIzaSyBVUi-vAyvE-g_iK4W9zbTTa5Ps8MwZEtg",
+    authDomain: "ca-final-537aa.firebaseapp.com",
+    projectId: "ca-final-537aa",
+    storageBucket: "ca-final-537aa.firebasestorage.app",
+    messagingSenderId: "156489737512",
+    appId: "1:156489737512:web:f8b06ec7cba71ddfe99c1d"
 };
 
 // Initialize Firebase
@@ -35,21 +30,17 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-// Export modules
+// Export everything
 export { 
-  auth, 
-  db, 
-  createUserWithEmailAndPassword, 
-  signInWithEmailAndPassword,
-  onAuthStateChanged,
-  signOut,
-  collection,
-  addDoc,
-  getDocs,
-  updateDoc,
-  doc,
-  query,
-  where,
-  setDoc,
-  getDoc
+    app,
+    auth, 
+    db, 
+    createUserWithEmailAndPassword, 
+    signInWithEmailAndPassword,
+    onAuthStateChanged,
+    signOut,
+    doc,
+    setDoc,
+    getDoc,
+    updateDoc
 };
